b1d6733925f9266fbaef340e1950b5867a87249c
Hayden Rodriguez <haydeno54@yahoo.com>
2020-11-05 19:26:35 -1000

    Added greeting method

--- demo/hello.java
+++ demo/hello.java
@@ -4,6 +4,10 @@ public class hello {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        greeting("Hi, how are you today?);
     }
-    
+
+    void greeting(String msg){
+        System.out.println(msg);
+    }
 }
