f01c61b40ce119244271387b31fee834d87befb0
Hayden Rodriguez <haydeno54@yahoo.com>
2020-11-05 19:31:48 -1000

    Added helper file

---
+++ demo/helper.java
@@ -0,0 +1,9 @@
+package csci2912-labdemo;
+
+public class helper{
+
+    public static void main(String[] args) {
+        System.out.println("Remember to always double check your work before you push it.");
+    }
+
+}
